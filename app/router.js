adbe74243756bdd2834e7935435bc0bd7083e35c
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-03-04 16:11:59 +0200

    [LRUG-3] job page and variabes.scss

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.resource('jobs');
+  this.resource('job', { path: 'jobs/:jobs_id' });
 });
 
 export default Router;
