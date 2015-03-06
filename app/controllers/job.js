487f589c5ec742ca1496a25238877a4fa5b3f1a4
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-03-06 18:09:34 +0200

    [LRUG-5] pagination

--- app/controllers/job.js
+++ app/controllers/job.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  needs: ['jobs'],
+
+  setJobs: function() {
+    var that = this;
+    var currentJobID = that.get("id");
+
+    var jobs = this.store.find('jobs')
+        .then(function(data) {
+          that.set("jobs", data);
+        });
+
+
+
+  }.observes("id").on("init")
+
 });
