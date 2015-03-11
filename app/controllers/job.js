76ab49523d4f68fef5613c3432e47ce1ec4e025f
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-03-11 18:19:58 +0200

    [LRUG-5] pagination job page

--- app/controllers/job.js
+++ app/controllers/job.js
@@ -3,15 +3,57 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   needs: ['jobs'],
 
-  setJobs: function() {
-    var that = this;
+  nextPage: function() {
+    var nextPage,
+        that = this;
 
     var jobs = this.store.find('jobs')
         .then(function(data) {
           that.set("jobs", data);
 
+          that.get('jobs')
+            .forEach(function (job, index) {
+              var currentJob = that.get("id");
+
+              if(++index == currentJob){
+
+                if(index < data.content.length){
+                  that.set("nextPage", ++currentJob);
+                } else {
+                  that.set("nextPage", false);
+                }
+
+              }
+
+            });
+
         });
+  }.on("init"),
 
-  }.observes("id").on("init")
+  prevPage: function() {
+    var prevPage,
+        that = this;
 
+    var jobs = this.store.find('jobs')
+        .then(function(data) {
+          that.set("jobs", data);
+
+          that.get('jobs')
+            .forEach(function (job, index) {
+              var currentJob = that.get("id");
+
+              if(++index == currentJob){
+
+                if(index < 0){
+                  that.set("prevPage", --currentJob);
+                } else {
+                  that.set("prevPage", false);
+                }
+
+              }
+
+            });
+
+        });
+  }.on("init")
 });
