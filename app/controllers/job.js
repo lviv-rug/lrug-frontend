e978a87ac73a240c81a7c942ba4805dc3be186ff
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-03-12 11:33:14 +0200

    [LRUG-5] pagination

--- app/controllers/job.js
+++ app/controllers/job.js
@@ -3,49 +3,31 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
   needs: ['jobs'],
 
-  nextPage: function() {
-    var nextPage,
-        that = this;
+  getJobs: function() {
+    var that = this;
 
     var jobs = this.store.find('jobs')
         .then(function(data) {
           that.set("jobs", data);
 
+          var jobsLength = that.get("jobs.length"),
+              currentJob = parseInt(that.get("id")),
+              nextJob = currentJob+1,
+              prevJob = currentJob-1;
+
           that.get('jobs')
             .forEach(function (job, index) {
-              var currentJob = that.get("id");
 
-              if(++index == currentJob){
+              if(++index === currentJob){
 
-                if(index < data.content.length){
-                  that.set("nextPage", ++currentJob);
+                if(index < jobsLength){
+                  that.set("nextPage", nextJob);
                 } else {
                   that.set("nextPage", false);
                 }
 
-              }
-
-            });
-
-        });
-  }.on("init"),
-
-  prevPage: function() {
-    var prevPage,
-        that = this;
-
-    var jobs = this.store.find('jobs')
-        .then(function(data) {
-          that.set("jobs", data);
-
-          that.get('jobs')
-            .forEach(function (job, index) {
-              var currentJob = that.get("id");
-
-              if(++index == currentJob){
-
-                if(index < 0){
-                  that.set("prevPage", --currentJob);
+                if(index > 0){
+                  that.set("prevPage", prevJob);
                 } else {
                   that.set("prevPage", false);
                 }
@@ -55,5 +37,17 @@ export default Ember.ObjectController.extend({
             });
 
         });
-  }.on("init")
+  }.observes("model").on("init"),
+
+  prevJob : function() {
+    if (this.get("prevPage") !== undefined) {
+      return this.store.find("jobs", this.get("prevPage"));
+    }
+  }.property("prevPage"),
+
+  nextJob : function() {
+    if (this.get("nextPage") !== undefined) {
+      return this.store.find("jobs", this.get("nextPage"));
+    }
+  }.property("nextPage")
 });
