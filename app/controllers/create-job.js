c1c916a3d6a49f893ac5d50a3f34448ba159b044
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-03-18 18:50:41 +0200

    [LRUG-6] create job

--- app/controllers/create-job.js
+++ app/controllers/create-job.js
@@ -1,4 +1,45 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+  needs: ['jobs'],
+
+  actions: {
+    createJob: function() {
+      var that = this;
+
+      this.store.find('jobs')
+        .then(function(data) {
+          that.set("jobs", data);
+
+          var position = that.get("position");
+          var company = that.get("company");
+          var link = that.get("link");
+          var state = that.get("state");
+          var salary = that.get("salary");
+          var email = that.get("email");
+          var description = that.get("description");
+
+          var jobs = that.store.createRecord('jobs', {
+            position: position,
+            company: company,
+            city: state,
+            price: salary,
+            email: email,
+            description: description
+          });
+
+
+          jobs.save();
+
+
+
+        });
+
+
+
+
+    }
+
+  }
+
 });
--- app/models/jobs.js
+++ app/models/jobs.js
@@ -1,10 +1,12 @@
 import DS from 'ember-data';
 
 var Jobs = DS.Model.extend({
-  title: DS.attr(),
+  position: DS.attr(),
+  company: DS.attr(),
   city: DS.attr(),
   price: DS.attr(),
   jobTime: DS.attr(),
+  email: DS.attr(),
   smallDescription: DS.attr(),
   description: DS.attr()
 });
@@ -13,28 +15,34 @@ Jobs.reopenClass({
   FIXTURES: [
     {
       id: 1,
-      title: "123",
+      position: "123",
+      company: 'Qwerty',
       city: 'Київ',
       price: '3500',
       jobTime: true,
+      email: '',
       smallDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?'
    },
    {
       id: 2,
-      title: "Senior Ruby Developer в GlobalLogic",
+      position: "Senior Ruby Developer",
+      company: 'GlobalLogic',
       city: 'Львів',
       price: '3500',
       jobTime: true,
+      email: '',
       smallDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?'
     },
     {
       id: 3,
-      title: "Senior Ruby Developer в GlobalLogic",
+      position: "Senior Ruby Developer",
+      company: 'GlobalLogic',
       city: 'Луцьк',
       price: '3500',
       jobTime: false,
+      email: '',
       smallDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?',
       description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas?'
   }
