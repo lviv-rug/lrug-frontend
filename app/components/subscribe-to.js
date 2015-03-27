45ea968362f045b44c0344376ebdcd1b4700c8c1
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-03-27 11:22:22 +0200

    [LRUG-5] subscription in sidebar

--- app/components/subscribe-to.js
+++ app/components/subscribe-to.js
@@ -1,29 +1,37 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
   actions: {
     subscribe: function() {
+      var that = this;
 
-      if(localStorage.subscribeEmail === undefined) {
+      var email = this.get("subscribeEmail");
+      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-        var subscribeEmail = [];
-        var lastEmail = {'id': 1,'email': this.get("subscribeEmail")};
+      if (re.test(email)) {
+        $('.error').removeClass('show').addClass('hide');
+        $('.success').removeClass('hide').addClass('show');
+
+        if(localStorage.subscribeEmail === undefined) {
+          var subscribeEmail = [];
+          var lastEmail = {'id': 1,'email': email};
+        } else {
+          var subscribeEmail = JSON.parse(localStorage.subscribeEmail);
+          var arrayLength = subscribeEmail.length;
+          var lastEmail = {'id': ++arrayLength,'email': email};
+        }
 
         subscribeEmail.push(lastEmail);
         localStorage.subscribeEmail = JSON.stringify(subscribeEmail)
+
+        setTimeout(function() {
+          that.set('subscribeEmail', '');
+          $('.success').removeClass('show').addClass('hide');
+        }, 1000);
 
       } else {
-
-        var subscribeEmail = JSON.parse(localStorage.subscribeEmail);
-        var arrayLength = subscribeEmail.length;
-        var lastEmail = {'id': ++arrayLength,'email': this.get("subscribeEmail")};
-
-        subscribeEmail.push(lastEmail);
-        localStorage.subscribeEmail = JSON.stringify(subscribeEmail)
-
+        $('.error').removeClass('hide').addClass('show');
       }
-
     }
   }
 });
