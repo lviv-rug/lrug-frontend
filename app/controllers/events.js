c2fbe933fd1b3962ccfea85d331c6976ba256c43
anna-hirnyk <anna.hirnyk@teamvoy.com>
2015-04-01 17:41:47 +0300

    [LRUG-7] list events

--- app/controllers/events.js
+++ app/controllers/events.js
@@ -1,5 +1,64 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  sortProperties: ['date'],
+  sortAscending: true,
 
+  events: function() {
+    var that = this;
+
+    this.store.find('events')
+      .then(function(data) {
+        that.set("events", data);
+
+          var date = that.get('events').map(function(event){
+            return event.get('date');
+          });
+
+          that.set('eventDate', date||[]);
+/////////////////////////////////////////////
+
+
+          var arrayDate = new Array(that.get('eventDate'));
+          var q1 = $(arrayDate)[0];
+
+          var sortDate  = $.unique(q1);
+
+          sortDate.sort().reverse().filter(function(el,i,a){
+            return i==a.indexOf(el);
+          });
+
+
+/////////////////////////////////////////////
+
+          var sortEventsLength = sortDate.length+1;
+          var q3 = [];
+          var dateArray = [];
+
+          for(var i = 0; i < sortEventsLength; i++ ) {
+
+            var q2 = that.get('events')
+              .forEach(function (event, index) {
+
+                if(event.get('date') === sortDate[i] ){
+                  dateArray.push(new Date(sortDate[i]).toString())
+
+                  q3.push(event);
+                  return q3;
+                }
+
+                that.set('sortEvents', q3);
+
+                that.set('dateArray', dateArray)
+                // console.log(dateArray)
+
+
+
+              });
+
+            }
+
+      });
+
+  }.on('init')
 });
--- app/models/events.js
+++ app/models/events.js
@@ -20,7 +20,7 @@ Events.reopenClass({
       city: 'Київ',
       company: 'Railsware',
       position: 'Senior Ruby Developer',
-      date: '25 березня 2014 року',
+      date: '2012-12-05T00:00:00.000Z',
       imageUser: 'http://images.clipartpanda.com/happy-computer-user-happy-computeruser.png',
       description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><ul><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum</li><li>Lorem ipsum dolor sit amet</li></ul> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
     },
@@ -31,7 +31,7 @@ Events.reopenClass({
       city: 'Київ',
       company: 'Railsware',
       position: 'Senior Ruby Developer',
-      date: '25 березня 2014 року',
+      date: '2014-03-25T00:00:00.000Z',
       imageUser: 'http://itexperts.co.za/wp-content/uploads/2014/10/computer-user-2.jpg',
       description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><ul><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum</li><li>Lorem ipsum dolor sit amet</li></ul><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
     },
@@ -42,7 +42,7 @@ Events.reopenClass({
       city: 'Київ',
       company: 'GlobalLogic',
       position: 'Senior Ruby Developer',
-      date: '25 березня 2014 року',
+      date: '2013-03-18T00:00:00.000Z',
       imageUser: 'http://itexperts.co.za/wp-content/uploads/2014/10/computer-user-2.jpg',
       description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><ul><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum</li><li>Lorem ipsum dolor sit amet</li></ul> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
     },
@@ -53,7 +53,7 @@ Events.reopenClass({
       city: 'Львів',
       company: 'Railsware',
       position: 'Senior Ruby Developer',
-      date: '25 березня 2014 року',
+      date: '2014-03-25T00:00:00.000Z',
       imageUser: 'http://images.clipartpanda.com/happy-computer-user-happy-computeruser.png',
       description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
     },
@@ -64,7 +64,40 @@ Events.reopenClass({
       city: 'Київ',
       company: 'GlobalLogic',
       position: 'Senior Ruby Developer',
-      date: '25 березня 2014 року',
+      date: '2014-03-25T00:00:00.000Z',
+      imageUser: '',
+      description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
+    },
+    {
+      id: 6,
+      title: "Senior Ruby Developer в GlobalLogic",
+      author: 'Олексій Васильєв',
+      city: 'Київ',
+      company: 'GlobalLogic',
+      position: 'Senior Ruby Developer',
+      date: '2013-03-18T00:00:00.000Z',
+      imageUser: 'http://itexperts.co.za/wp-content/uploads/2014/10/computer-user-2.jpg',
+      description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><ul><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum dolor sit amet</li><li>Lorem ipsum</li><li>Lorem ipsum dolor sit amet</li></ul> <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
+    },
+    {
+      id: 7,
+      title: "Senior Ruby Developer",
+      author: 'Олексій Васильєв',
+      city: 'Львів',
+      company: 'Railsware',
+      position: 'Senior Ruby Developer',
+      date: '2014-03-25T00:00:00.000Z',
+      imageUser: 'http://images.clipartpanda.com/happy-computer-user-happy-computeruser.png',
+      description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
+    },
+    {
+      id: 8,
+      title: 'Lorem ipsum dolor sit amet',
+      author: 'Олексій Васильєв',
+      city: 'Київ',
+      company: 'GlobalLogic',
+      position: 'Senior Ruby Developer',
+      date: '2013-03-18T00:00:00.000Z',
       imageUser: '',
       description: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, quod. Animi natus, ipsa eveniet quos laborum iusto voluptate voluptas? Ducimus earum incidunt ratione reiciendis nisi, aperiam impedit nihil dignissimos ad?</p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget odio.</p>'
     }
